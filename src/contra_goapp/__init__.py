c73892bc6ea1fd3e764d0f00952114dcf2c2d324
DanO <dordonez@usv.edu>
2021-11-07 13:54:18 -0800

    "Test changes"

--- src/contra_goapp/__init__.py
+++ src/contra_goapp/__init__.py
@@ -10,8 +10,6 @@ try:
     # Change here if project is renamed and does not equal the package name
     dist_name = "CONTRA_GOAPP"
     __version__ = version(dist_name)
-
-
 except PackageNotFoundError:  # pragma: no cover
     __version__ = "unknown"
 finally:
